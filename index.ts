ad0fc1f1698afda612645b31b6c2436722670e4d
Anwesh43 <anweshthecool0@gmail.com>
2021-05-22 20:29:54 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -13,6 +13,8 @@ const colors : Array<string> = [
     "#FFD600"
 ] 
 const backColor : string = "#bdbdbd"
+const w: number = window.innerWidth 
+const h : number = window.innerHeight
 
 class ScaleUtil {
 
@@ -26,5 +28,55 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawStrokedCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number, scale : number) {
+        context.beginPath()
+        for (var j = 0; j <= deg; j++) {
+            const a : number = x + r * Math.cos(j * Math.PI / 180)
+            const b : number = y + r * Math.sin(j * Math.PI / 180)
+            if (j == 0) {
+                context.moveTo(a, b)
+            } else {
+                context.lineTo(a, b)
+            }
+        }
+        context.stroke()
+    }
+
+    static drawCircleEdgeLine(context : CanvasRenderingContext2D, scale : number) {
+        const r : number = Math.min(w, h) / sizeFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(rot * sf3)
+        DrawingUtil.drawStrokedCircle(context, 0, 0, r, sf1)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.translate(-r, -r)
+            DrawingUtil.drawLine(context, 0, 0, 0, -r * sf2)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawCERANode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawCircleEdgeLine(context, scale)
     }
 }
