f72e58acd14e02d035d3f68df55735ad391b0b5c
Anwesh43 <anweshthecool0@gmail.com>
2021-05-22 18:59:08 +0530

    adding contants

---
+++ index.ts
@@ -0,0 +1,15 @@
+const parts : number = 4 
+const scGap : number = 0.02 / parts 
+const deg : number = 360 
+const rot : number = Math.PI / 2 
+const strokeFactor : number = 90 
+const sizeFactor : number = 4.9 
+const delay : number = 20 
+const colors : Array<string> = [
+    "#f44336",
+    "#4A148C",
+    "#00C853",
+    "#AA00FF",
+    "#FFD600"
+] 
+const backColor : string = "#bdbdbd"
